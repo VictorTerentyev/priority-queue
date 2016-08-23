af81cb101cb99ca3165d477d25168d41f9778199
zerdred <m.geekstore@gmail.com>
2016-08-23 19:31:24 +0300

    ShiftNodeUp/ShiftNodeDown/AppendNode
    fixes for mentioned functions

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,13 +1,15 @@
 const Node = require('./node');
 
 class MaxHeap {
+	
 	constructor() {
 		this.root = null;
 		this.parentNodes = [];
+		this.supportNodes = [];
 		this.size = 0;
 		this.lastInsertedNode = null;
 		this.currentNode = null;
-		this.rec = 0;
+		this.counter = 0;
 	}
 
 	push(data, priority) {
@@ -16,26 +18,30 @@ class MaxHeap {
 		this.shiftNodeUp(node);
 	}
 
-	pop() {
-		this.size--;
+	pop() { 
 		if (this.root != null) {
-			let data = this.root;
 			this.restoreRootFromLastInsertedNode(this.detachRoot());
-			return data;	
+			if (this.root != null) {
+				this.shiftNodeDown(this.root);
+			}
+			this.size--;
+			return this.root; //temporary hack
 		}
 	}
 
 	detachRoot() {
-		let root = this.lastInsertedNode;
+		let root = this.root;
 		this.parentNodes.shift();
 		this.root = null;
 		return root;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		this.parentNodes[-1] = this.parentNodes[0];
-		this.parentNodes[1] = this.parentNodes[0];
-		this.parentNodes[0] = this.lastInsertedNode;
+		let root = detached;
+		let lastInsertedNode = this.parentNodes.pop();
+		this.parentNodes.unshift(lastInsertedNode);
+		this.root = lastInsertedNode;
+		return this.root;	
 	}
 
 	size() { 
@@ -53,52 +59,110 @@ class MaxHeap {
 	clear() {
 		this.root = null;
 		this.parentNodes = [];
+		this.supportNodes = [];
 		this.size = 0;
 		this.lastInsertedNode = null;
-		this.rec = 0;
+		this.currentNode = null;
+		this.counter = 0;
 	}
 
 	insertNode(node) {
+		this.findCurrentNode();
 		if (this.size == 0) {
 			node.parent = null;
+			node.left = null;
+			node.right = null;
 			this.root = node;
 			this.parentNodes.push(node);
-			this.currentNode = node;
 		} else {
-			this.root.appendChild(node);
 			this.parentNodes.push(node);
-			for (var i = 0; i < this.parentNodes.length; i++) {
-				if(this.parentNodes[i] == this.currentNode) {
-					if (this.parentNodes[i].left == null) {
-						for (var j = 1; j < this.parentNodes.length; j++) {
-							if (this.parentNodes[j].parent == null) {
-								this.parentNodes[i].left = this.parentNodes[j];
-								this.parentNodes[j].parent = this.parentNodes[i];
-							}
-						}
-					} else if (this.parentNodes[i].right == null) {
-						for (var k = 1; k < this.parentNodes.length; k++) {
-							if (this.parentNodes[k].parent == null) {
-								this.parentNodes[i].right = this.parentNodes[k];
-								this.parentNodes[k].parent = this.parentNodes[i];
-							}
-						}
-					} else {
-						this.currentNode = this.parentNodes[i+1];
-					}
-				}
-			}
+			this.appendNode(node);
+			this.parentNodes[0].parent = null;
 		}
 		this.lastInsertedNode = this.parentNodes[this.parentNodes.length-1];
 		this.size++;
 	}
 
 	shiftNodeUp(node) {
-		node.swapWithParent();
+		if (this.root != node) {
+			if (node.parent != this.root) {
+				node.swapWithParent();
+				this.shiftNodeUp(node);
+			}
+		}
 	}
 
 	shiftNodeDown(node) {
-		node.left.swapWithParent();
+		if (node.left != null) {
+			node.left.swapWithParent();
+			this.shiftNodeDown(node);
+		}
+	}
+
+	appendNode(node) {
+		this.parentNodes[this.parentNodes.length-1].parent = null;
+		for (var i = 0; i < this.parentNodes.length; i++) {
+			if (this.parentNodes[i] == this.currentNode) {
+				if (this.counter == 0 ) {
+					this.parentNodes[i].left = null;
+					this.counter += 2;
+				} else if (this.counter == 1 || this.counter == 2) {
+					if (this.counter == 1) {
+						this.parentNodes[i].left = null;
+						this.counter += 2;
+					} else if (this.counter == 2) {
+						this.parentNodes[i].left.left = null;
+						this.parentNodes[i].right = null;
+						this.counter++;
+					}
+				} else {
+					this.findCurrentNode();
+					this.appendNode(node);
+					break;
+				}
+				if (this.parentNodes[i].left == null) {
+					for (var j = 0; j < this.parentNodes.length; j++) {
+						if (this.parentNodes[j].parent == null && j != 0) {
+							this.parentNodes[i].left = this.parentNodes[j];
+							this.parentNodes[j].parent = this.parentNodes[i];
+							break;
+						}
+					}
+					break;
+				} else if (this.parentNodes[i].right == null ) {
+					for (var k = 0; k < this.parentNodes.length; k++) {
+						if (this.parentNodes[k].parent == null && k != 0) {
+							this.parentNodes[i].right = this.parentNodes[k];
+							this.parentNodes[k].parent = this.parentNodes[i];
+							break;
+						}
+					}
+					break;
+				}
+			}
+		}
+	}
+
+	findCurrentNode() {
+		if (this.size != 0) {
+			for (var i = 0; i < this.parentNodes.length; i++) {
+				if (this.parentNodes[i].left == null && this.parentNodes[i].right == null) {
+					this.currentNode = this.parentNodes[i];
+					this.counter = 0;
+					break;
+				} else if (this.parentNodes[i].left == null && this.parentNodes[i].right != null) {
+					this.currentNode = this.parentNodes[i];
+					this.counter = 1;
+					break;
+				} else if (this.parentNodes[i].left != null && this.parentNodes[i].right == null) {
+					this.currentNode = this.parentNodes[i];
+					this.counter = 2;
+					break;
+				}
+			}
+		} else {
+			this.currentNode = null;
+		}
 	}
 }
 
